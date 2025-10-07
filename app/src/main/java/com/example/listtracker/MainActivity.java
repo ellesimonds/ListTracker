54f423d868787bfba3a50ffc8f9171028f6e8505
Elle Simonds <ellesimonds@gmail.com>
2025-10-07 11:47:25 -0500

    all app features added

---
+++ app/src/main/java/com/example/listtracker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.listtracker;
+
+import android.widget.ListView;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class MainActivity {
+    public MainActivity() {
+        ListView listOfTasks;
+
+        ViewModel view  = new AndroidViewModel(this.getClass())
+
+    }
+
+
+}
